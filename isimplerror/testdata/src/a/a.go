fc1d0ad5c2a8891b79f009d9af0401690dd0394e
taiyoslime <t@iyosli.me>
2020-09-02 15:53:45 +0900

    improve tests

--- isimplerror/testdata/src/a/a.go
+++ isimplerror/testdata/src/a/a.go
@@ -2,15 +2,21 @@ package a
 
 type myError struct { // want "OK"
 }
+
 func (e myError) Error() string {
 	return ""
 }
+
 type myError2 struct { // want "OK"
 }
 
 func (e *myError2) Error() string {
 	return ""
 }
-type hoge struct {
 
+type hoge struct {
+}
+
+type myError3 struct { // want "OK"
+	myError
 }
